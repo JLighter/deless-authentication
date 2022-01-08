5098f10920477e6b46b4474c7ee8769a29c34869
JLighter <heroguelle.julien@gmail.com>
2022-01-08 02:57:41 +0100

    Change token content to email

--- services/token/token.go
+++ services/token/token.go
@@ -9,7 +9,7 @@ import (
 	"github.com/golang-jwt/jwt/v4"
 )
 
-func createClaim(username string, admin bool) (*jwt.MapClaims, error) {
+func createClaim(email string, admin bool) (*jwt.MapClaims, error) {
   token_expire, err := config.Config("TOKEN_EXPIRE")
   if err != nil {
     log.Fatalf("cannot get TOKEN_EXPIRE variable : %s", err)
@@ -21,14 +21,14 @@ func createClaim(username string, admin bool) (*jwt.MapClaims, error) {
   }
 
   return &jwt.MapClaims{
-		"name":  username,
+		"email":  email,
 		"admin": admin,
 		"exp":   time.Now().Add(time.Second * time.Duration(expire)).Unix(),
 	}, nil
 }
 
-func GenerateToken(username string, admin bool) string {
-  claim, err := createClaim(username, admin)
+func GenerateToken(email string, admin bool) string {
+  claim, err := createClaim(email, admin)
   if err != nil {
     log.Fatalf("cannot generate claim: %s", err)
   }
