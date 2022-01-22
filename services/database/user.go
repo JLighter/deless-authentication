1dfcc21b4a622c59a71e38ef66e9eb30ffa88d39
JLighter <heroguelle.julien@gmail.com>
2022-01-22 10:32:29 +0100

    Users are managed in mongodb instead of redis

--- services/database/user.go
+++ services/database/user.go
@@ -2,42 +2,37 @@ package database
 
 import (
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
+const DATABASE_NAME = "AUTH"
+const USERS_COLLECTION = "USERS"
+const PASSWORD_COLLECTION = "PASSWORDS"
+
 type User struct {
+  Id       string `json:"id" binding:"required"`
 	Username string `json:"username" binding:"required"`
 	Email    string `json:"email" binding:"required"`
   Admin    bool `json:"-"`
 }
 
-type NewUser struct {
-	Email    string
-	Password string
-	Username string
-  Admin    bool
+type Password struct {
+  UserId       string `json:"id" binding:"required"`
+  Value     string `json:"value" binding:"required"`
 }
 
-func userKey(email string) string {
-  return fmt.Sprintf("user:%v", email)
-}
-
-func userPasswordKey(email string) string {
-  return fmt.Sprintf("%v:password", userKey(email))
-}
-
-func (d *Database) RegisterUser(user NewUser) error {
+func (d *MongoDB) RegisterUser(user User) error {
   client, err := d.connect()
   if err != nil {
     return fmt.Errorf("Error connecting to database: %v", err)
   }
-	defer client.Close()
+	defer d.disconnect(client)
 
-	pipeline := client.Pipeline()
+  database := client.Database(DATABASE_NAME)
+  users := database.Collection(USERS_COLLECTION)
 
-	pipeline.HMSet(d.ctx, userKey(user.Email), "username", user.Username, "admin", user.Admin)
-	pipeline.Set(d.ctx, userPasswordKey(user.Email), user.Password, 0)
+  _, err = users.InsertOne(d.ctx, user)
 
-	_, err = pipeline.Exec(d.ctx)
 	if err != nil {
 		return fmt.Errorf("error saving user: %v", err)
 	}
@@ -45,45 +40,55 @@ func (d *Database) RegisterUser(user NewUser) error {
   return nil
 }
 
-// Compare password with the hash stored in the Database
-func (d *Database) ComparePassword(email string, password string) (bool, error) {
+func (d *MongoDB) ComparePassword(toCompare Password) (bool, error) {
   client, err := d.connect()
   if err != nil {
     return false, fmt.Errorf("Error connecting to database: %v", err)
   }
-  defer client.Close()
+	defer d.disconnect(client)
 
-  result, err := client.Get(d.ctx, userPasswordKey(email)).Result()
-  if err != nil || result != password {
+  database := client.Database(DATABASE_NAME)
+  passwords := database.Collection(PASSWORD_COLLECTION)
+
+  var password Password;
+
+  err = passwords.FindOne(d.ctx, bson.M{"UserId": toCompare.UserId}).Decode(&password)
+  if err != nil || toCompare.Value != password.Value {
     return false, nil
   }
 
   return true, nil
 }
 
-func (d *Database) ChangePassword(email string, password string) error {
+func (d *MongoDB) ChangePassword(newPassword Password) error {
   client, err := d.connect()
   if err != nil {
     return fmt.Errorf("Error connecting to database: %v", err)
   }
-	defer client.Close()
+	defer d.disconnect(client)
 
-	_, err = client.Set(d.ctx, userPasswordKey(email), password, 0).Result()
+  database := client.Database(DATABASE_NAME)
+  passwords := database.Collection(PASSWORD_COLLECTION)
+
+  _, err = passwords.ReplaceOne(d.ctx, bson.M{"UserId": newPassword.UserId}, newPassword)
 	if err != nil {
-    return fmt.Errorf("Error updating database: %v", err)
+    return fmt.Errorf("Error updating password: %v", err)
 	}
 
 	return nil
 }
 
-func (d *Database) UpdateUser(user User) error {
-	client, err := d.connect()
+func (d *MongoDB) UpdateUser(user User) error {
+  client, err := d.connect()
   if err != nil {
     return fmt.Errorf("Error connecting to database: %v", err)
   }
-	defer client.Close()
+	defer d.disconnect(client)
 
-	_, err = client.HMSet(d.ctx, userKey(user.Email), "username", user.Username).Result()
+  database := client.Database(DATABASE_NAME)
+  users := database.Collection(USERS_COLLECTION)
+
+  _, err = users.ReplaceOne(d.ctx, bson.M{"id": user.Id}, user)
 	if err != nil {
 		return fmt.Errorf("Error updating database: %v", err)
 	}
@@ -91,21 +96,21 @@ func (d *Database) UpdateUser(user User) error {
 	return nil
 }
 
-func (d *Database) GetUser(email string) (*User, error) {
-	client, err := d.connect()
+func (d *MongoDB) GetUser(id string) (*User, error) {
+  client, err := d.connect()
   if err != nil {
     return nil, fmt.Errorf("Error connecting to database: %v", err)
   }
-	defer client.Close()
+	defer d.disconnect(client)
 
-	result, err := client.HMGet(d.ctx, userKey(email), "username", "admin").Result()
+  database := client.Database(DATABASE_NAME)
+  users := database.Collection(USERS_COLLECTION)
+
+  var user User;
+  err = users.FindOne(d.ctx, bson.M{"id": id}).Decode(&user)
 	if err != nil {
 		return nil, nil 
 	}
 
-	return &User{
-		Username: result[0].(string),
-		Admin:    result[1].(string) == "true",
-		Email:    email,
-	}, nil
+	return &user, nil
 }
