423a59cffcab61a1fc8b606eeb7ef0d0cdddb049
JLighter <heroguelle.julien@gmail.com>
2022-01-23 22:39:46 +0100

    Encrypt passwords with bcrypt

--- services/database/password.go
+++ services/database/password.go
@@ -6,6 +6,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Password struct {
@@ -21,13 +22,28 @@ func (d *MongoDB) getPasswordCollection() *mongo.Collection {
   return passwords
 }
 
+// Hash password using bcrypt
+func (d *MongoDB) hashPassword(password string) (string, error) {
+  hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+  if err != nil {
+    return "", fmt.Errorf("Error hashing password: %v", err)
+  }
+
+  return string(hashedPassword), nil
+}
+
 func (d *MongoDB) ComparePassword(toCompare Password) (bool, error) {
   passwords := d.getPasswordCollection()
 
   var password Password;
 
   err := passwords.FindOne(d.ctx, bson.M{"userid": toCompare.UserId}).Decode(&password)
-  if err != nil || toCompare.Value != password.Value {
+  if err != nil {
+    return false, nil
+  }
+
+  err = bcrypt.CompareHashAndPassword([]byte(password.Value), []byte(toCompare.Value))
+  if err != nil {
     return false, nil
   }
 
@@ -36,10 +52,17 @@ func (d *MongoDB) ComparePassword(toCompare Password) (bool, error) {
 
 func (d *MongoDB) ChangePassword(newPassword Password) error {
   passwords := d.getPasswordCollection()
+  hashedPasswordValue, err := d.hashPassword(newPassword.Value)
 
-  _, err := passwords.ReplaceOne(d.ctx, bson.M{"userid": newPassword.UserId}, newPassword)
+  if err != nil {
+    return fmt.Errorf("error hashing password: %v", err)
+  }
+
+  newPassword.Value = hashedPasswordValue
+
+  _, err = passwords.ReplaceOne(d.ctx, bson.M{"userid": newPassword.UserId}, newPassword)
 	if err != nil {
-    return fmt.Errorf("Error updating password: %v", err)
+    return fmt.Errorf("error updating password: %v", err)
 	}
 
 	return nil
@@ -47,10 +70,17 @@ func (d *MongoDB) ChangePassword(newPassword Password) error {
 
 func (d *MongoDB) InsertPassword(password Password) error {
   passwords := d.getPasswordCollection()
+  hashedPasswordValue, err := d.hashPassword(password.Value)
 
-  _, err := passwords.InsertOne(d.ctx, password)
   if err != nil {
-    return fmt.Errorf("Error setting password: %v", err)
+    return fmt.Errorf("error hashing password: %v", err)
+  }
+
+  password.Value = hashedPasswordValue
+
+  _, err = passwords.InsertOne(d.ctx, password)
+  if err != nil {
+    return fmt.Errorf("error setting password: %v", err)
   }
 
   return nil
