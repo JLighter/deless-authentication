ebd268302877aebf9eb0bebbcd4fde0ac3948e39
JLighter <heroguelle.julien@gmail.com>
2022-01-23 18:23:58 +0100

    Remove commentary on config.go

--- config/config.go
+++ config/config.go
@@ -7,9 +7,7 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// Config func to get env value
 func Config(key string) (string, error) {
-	// load .env file
 	err := godotenv.Load(".env")
 	if err != nil {
     return "", fmt.Errorf("error loading .env file : %s", err)
